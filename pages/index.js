e2bb68df73fb782ef323eac9096a79152db618f5
Pavan <121234035+pavan-chintala@users.noreply.github.com>
2024-06-22 16:44:08 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -60,15 +60,15 @@ export default function Home({ posts }) {
               </RoughNotation>
               in between. Have a good read!{' '}
               <div className="mt-8 text-slate-600 dark:text-slate-400">
-                <span className="text-sm">Press</span>{' '}
+                <span className="text-sm"> </span>{' '}
                 <span className="rounded-md bg-gray-300 p-1 text-sm text-gray-900 dark:bg-gray-400">
-                  ⌘
+                   
                 </span>{' '}
-                <span className="text-sm">+ </span>
+                <span className="text-sm">  </span>
                 <span className="rounded-md bg-gray-300 p-1 text-sm text-gray-900 dark:bg-gray-400">
-                  K
+                   
                 </span>{' '}
-                <span className="text-sm">to start</span>
+                <span className="text-sm"> </span>
               </div>
             </p>
           </div>
